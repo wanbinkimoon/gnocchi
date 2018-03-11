638e4562a929cd31e2c983aeba98193c9e76ecc0
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-03-12 00:24:03 +0100

    exercise on the triangle

--- src/Scenes/scene5.js
+++ src/Scenes/scene5.js
@@ -1,39 +1,38 @@
 export default  (s, p) => {
-  const points = {
-    a: {
-      x: 75, 
-      y: 100
-    }, 
-    b: {
-      x: 125, 
-      y: 100
-    }, 
-    c: {
-      x: 100, 
-      y: 58
-    }
+  const side = 125
+  const trianglePoint = {
+    x1: 100, 
+    y1: (Math.sqrt(3) / 3 * side * - 1) + 100, 
+    x2: (side / 2  * -1) + 100, 
+    y2: (Math.sqrt(3) / 6 * side) + 100, 
+    x3: (side / 2) + 100, 
+    y3: (Math.sqrt(3) / 6 * side) + 100, 
   }
+
+  const rectPoint = {
+    x: 100,
+    y: 100,
+    w: 100,
+    h: 100,
+  }
+  
   s.background(0)
-  s.stroke(255)
-  s.strokeWeight(1)
   s.noFill()
-  s.ellipse(points.a.x, points.a.y, 100, 100)
-  s.ellipse(points.b.x, points.b.y, 100, 100)
-  
-  s.stroke(255, 255, 125)
-  s.strokeWeight(2)
-  s.ellipse(points.a.x, points.a.y, 2, 2)
-  s.ellipse(points.b.x, points.b.y, 2, 2)
-  s.ellipse(points.c.x, points.c.y, 2, 2)
-  
   s.strokeWeight(1)
+  
+  s.stroke(255)
+  s.ellipse(100, 100, (side / Math.sqrt(3)) * 2, (side / Math.sqrt(3)) * 2)
+  // s.rect()
+
+
   s.stroke(255, 100, 100)
   s.triangle(
-    points.a.x,
-    points.a.y,
-    points.b.x,
-    points.b.y,
-    points.c.x,
-    points.c.y,
-  )
+    trianglePoint.x1, 
+    trianglePoint.y1, 
+    trianglePoint.x2, 
+    trianglePoint.y2, 
+    trianglePoint.x3, 
+    trianglePoint.y3);
+
+
 }
